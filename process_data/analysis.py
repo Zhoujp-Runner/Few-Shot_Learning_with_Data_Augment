2183ac04ea4498777086d25eb6714533b88b26e6
Zhoujp <1228423514@qq.com>
2023-03-20 22:23:57 +0800

    add dim_decay function and fix a problem about no-using copy

--- process_data/analysis.py
+++ process_data/analysis.py
@@ -4,6 +4,7 @@
 """
 1. 使用LDA对数据集进行降维
 """
+import numpy as np
 import torch
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import dill
@@ -17,16 +18,17 @@ save_lda_path = "..\\processed_data\\data_after_lda.pkl"
 def transform_attribute_to_label(attribute, information):
     """
     将多维的属性矩阵转换成一维的标签矩阵
-    :param attribute: 属性矩阵 [batch_size, attribute_dim]，可以是tensor，也可以是ndarray
+    :param attribute: 属性矩阵 [batch_size, attribute_dim]，np.ndarray
     :param information: 每个属性具体的取值
     :return: 一维的标签矩阵
     """
     # 根据information中每个属性的取值顺序，重新赋值attribute
-    attribute = pd.DataFrame(attribute, index=None, columns=None)
+    attribute_copy = np.copy(attribute)
+    attribute_copy = pd.DataFrame(attribute_copy, index=None, columns=None)
     for item_idx, item in enumerate(information):
         for idx, value in enumerate(item):
-            attribute.loc[attribute[item_idx] == value, item_idx] = idx
-    attribute = torch.FloatTensor(attribute.values)
+            attribute_copy.loc[attribute_copy[item_idx] == value, item_idx] = idx
+    attribute_copy = torch.FloatTensor(attribute_copy.values)
 
     # 根据属性取值的范围计算系数（类似于进制转换）
     # 每一位都是任意进制，例如第一位是满2进1，到了第二位就是满3进1
@@ -40,7 +42,7 @@ def transform_attribute_to_label(attribute, information):
     information_coefficient[:-1] = information_prod_reverse[1:]
 
     # 矩阵乘法，计算标签值
-    label_mat = torch.matmul(attribute, information_coefficient)
+    label_mat = torch.matmul(attribute_copy, information_coefficient)
     return label_mat
 
 
@@ -62,6 +64,20 @@ def lda(data, label, dim_out=64):
     lda_model.fit(data, label)
     data_after_lda = lda_model.transform(data)
     return data_after_lda
+
+
+def dim_decay(data_dict, information):
+
+    attribute = data_dict["attribute"]
+    source_data = data_dict["data"]
+    label =transform_attribute_to_label(attribute, information)
+    data_after_lda = lda(source_data, label)
+    data_dict_af_lda = {
+        "data": data_after_lda,
+        "attribute": attribute
+    }
+    print(attribute)
+    return data_dict_af_lda
 
 
 if __name__ == '__main__':

877c3df02d1e034c8172694b5b46ee96a2ecda00
Zhoujp <1228423514@qq.com>
2023-03-25 22:09:48 +0800

    add pca method for dim_decay

--- process_data/analysis.py
+++ process_data/analysis.py
@@ -3,16 +3,22 @@
 # DATE: 21:32 2023/3/15
 """
 1. 使用LDA对数据集进行降维
+2. 使用PCA对数据集进行降维
 """
 import numpy as np
 import torch
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.decomposition import PCA
 import dill
 import pandas as pd
+import yaml
+from easydict import EasyDict
 
 
-save_path = "..\\processed_data\\data_dict.pkl"
-save_lda_path = "..\\processed_data\\data_after_lda.pkl"
+with open("..\\configs\\config_0.yaml") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+config = EasyDict(config)
 
 
 def transform_attribute_to_label(attribute, information):
@@ -66,31 +72,72 @@ def lda(data, label, dim_out=64):
     return data_after_lda
 
 
-def dim_decay(data_dict, information):
+def pca(data, dim_out=64):
+    """
+    使用主成分分析，对数据进行降维
+    :param data: 原始数据  [batch_size, dim]
+    :param dim_out: 降维后的维度
+    :return: 降维后的数据
+    """
+    pca_model = PCA(n_components=dim_out)
+    pca_model.fit(data)
+    data_after_pca = pca_model.fit_transform(data)
+    return data_after_pca
+
+
+def dim_decay(data_dict, information, method=None):
+    """
+    对数据进行降维
+    :param data_dict: 包含原始数据的字典
+    :param information: 数据类别信息
+    :param method: 用什么方法进行降维，可选"LDA"和"PCA"，当为None时，不对数据进行处理
+    :return: 包含降维后的数据的字典
+    """
+    if method is None:
+        print("There is no method, you should set a method for dim_decay!")
+        return data_dict
 
     attribute = data_dict["attribute"]
     source_data = data_dict["data"]
-    label =transform_attribute_to_label(attribute, information)
-    data_after_lda = lda(source_data, label)
-    data_dict_af_lda = {
-        "data": data_after_lda,
+    data_dim_decay = None
+
+    if method == 'LDA':
+        label = transform_attribute_to_label(attribute, information)
+        data_dim_decay = lda(source_data, label)
+    elif method == 'PCA':
+        data_dim_decay = pca(source_data)
+
+    data_dict_af_dim_decay = {
+        "data": data_dim_decay,
         "attribute": attribute
     }
-    print(attribute)
-    return data_dict_af_lda
+    return data_dict_af_dim_decay
 
 
 if __name__ == '__main__':
+    save_path = config.save_path
+    save_lda_path = config.save_lda_path
+    save_pca_path = config.save_pca_path
     with open(save_path, 'rb') as f:
         data = dill.load(f)
     attribute = data['attribute']
     information = [[3, 20, 100], [100, 90, 80, 73], [0, 1, 2], [130, 115, 100, 90]]
     label = transform_attribute_to_label(attribute, information)
-    source_data = data['data']
-    data_af_lda = lda(source_data, label)
-    data_dict_af_lda = dict()
-    data_dict_af_lda['data'] = data_af_lda
-    data_dict_af_lda['attribute'] = attribute
-    print(data_af_lda.shape)
-    with open(save_lda_path, 'wb') as f_lda:
-        dill.dump(data_dict_af_lda, f_lda)
+    # source_data = data['data']
+    # data_af_lda = lda(source_data, label)
+    # data_dict_af_lda = dict()
+    # data_dict_af_lda['data'] = data_af_lda
+    # data_dict_af_lda['attribute'] = attribute
+    # print(data_af_lda.shape)
+    # with open(save_lda_path, 'wb') as f_lda:
+    #     dill.dump(data_dict_af_lda, f_lda)
+    # from collections import Counter
+    # count = Counter(label.numpy())
+    # print(count)
+    # print(max(count.values()))
+    # data_after_dim_decay = dim_decay(data, information, method='PCA')
+    # with open(save_pca_path, 'wb') as f_pca:
+    #     dill.dump(data_after_dim_decay, f_pca)
+    with open(save_pca_path, 'rb') as r_pca:
+        data_dict = dill.load(r_pca)
+    print(data_dict['data'].shape)

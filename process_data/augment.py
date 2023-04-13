2a79575965937798609624640c29dc08ba48fcff
Zhoujp <1228423514@qq.com>
2023-04-13 15:01:21 +0800

    create the DataAugment to integrate the process of augment

--- process_data/augment.py
+++ process_data/augment.py
@@ -8,101 +8,154 @@ import os
 import yaml
 import dill
 import torch
+import logging
 import numpy as np
 from easydict import EasyDict
 from itertools import product
 
 from models.diffusion import DiffusionModel
-from models.model import MLPModel, ConcatModel
+from models.model import MLPModel, ConcatModel, AttentionModel
+from process_data.analysis import attribute_standard
 
 
-with open("..\\configs\\config_0.yaml") as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
+class DataAugment(object):
+    def __init__(self, config, epoch):
+        super(DataAugment, self).__init__()
+        self.config = config
+        self.epoch = epoch
+        self._set_log()
 
-config = EasyDict(config)
+    def _set_log(self):
+        """设置log文件"""
+        self.logger = logging.getLogger("AugmentLog")
+        self.logger.setLevel(logging.DEBUG)
 
+        # 清空该log的句柄
+        for handle in self.logger.handlers:
+            self.logger.removeHandler(handle)
 
-def data_augment(config, model_type):
-    """
-    使用扩散模型生成新数据，用于数据增强
-    :param config:
-    :param model_type:
-    :return:
-    """
-    # 生成模型加载路径
-    if model_type == "MLP":
-        epoch = 499
-        batch_size = config.batch_size
-        load_name = f'epoch{epoch}_bs{batch_size}_checkpoint.pkl'
-        load_root = config.save_model_root_path
-        shots_num = f"{config.shots_num}-shots"
-        load_path = os.path.join(load_root, model_type, shots_num, load_name)
+        # self.filehandle = logging.FileHandler(self.config.save_log_path)
+        file_root = self.config.augment_root
+        file_name = f"augment_{self.config.shots_num}_{self.config.method}.log"
+        file_path = os.path.join(file_root, file_name)
+        self.filehandle = logging.FileHandler(file_path)
+        self.filehandle.setLevel(logging.DEBUG)
 
-        # 加载预测模型
-        input_dim = 64
-        num_steps = config.num_diffusion_steps
-        model = MLPModel(input_dim, num_steps)
+        fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        self.formatter = logging.Formatter(fmt)
 
-        model_dict = torch.load(load_path)
-        model_state = model_dict['model_state_dict']
-        model.load_state_dict(model_state)
-    elif model_type == "ConcatLinear":
-        epoch = 599
-        load_name = f'epoch{epoch}_checkpoint.pkl'
-        load_root = config.save_model_root_path
-        sub_dir_name = "concat_linear3_with_standard_data"
-        load_path = os.path.join(load_root, model_type, sub_dir_name, load_name)
+        self.filehandle.setFormatter(self.formatter)
+        self.logger.addHandler(self.filehandle)
 
-        # 加载预测模型
-        model = ConcatModel(dim_condition=4)
+    def data_augment(self, dim_in, dim_condition, model_type, ways):
+        """
+        使用扩散模型生成新数据，用于数据增强
+        """
+        self.logger.info(f"model_type : {model_type}")
+        self.logger.info(f"augment_num : {self.config.augment_num}")
+        self.logger.info(f"loading epoch : {self.epoch}")
 
-        model_dict = torch.load(load_path)
-        model_state = model_dict['model_state_dict']
-        model.load_state_dict(model_state)
-    else:
-        raise ValueError("There is no such model_type, please try another type!")
+        # 生成模型加载路径
+        if model_type == "MLP":
+            load_name = f'epoch{self.epoch}_checkpoint.pkl'
+            load_root = self.config.diffusion_model_root
+            sub_dir_name = f"{self.config.shots_num}_{self.config.method}"
+            load_path = os.path.join(load_root, model_type, sub_dir_name, load_name)
 
-    # 加载扩散模型
-    diffusion_model = DiffusionModel(config)
+            # 加载预测模型
+            num_steps = self.config.num_diffusion_steps
+            model = MLPModel(dim_in, num_steps)
 
-    # 每一个属性需要扩增的数据的大小
-    augment_size = (config.augment_num, 64)  # TODO 这个64需要改一下，保持和那边降维的数据维度同步
+            model_dict = torch.load(load_path)
+            model_state = model_dict['model_state_dict']
+            model.load_state_dict(model_state)
 
-    # 生成所有类别
-    attributes = []
-    cooler, valve, pump, hydraulic = config.information
-    att_iter = product(cooler, valve, pump, hydraulic)
-    for item in att_iter:
-        attributes.append(list(item))
-    attributes = torch.FloatTensor(attributes)
+        elif model_type == "ConcatLinear":
+            load_name = f'epoch{self.epoch}_checkpoint.pkl'
+            load_root = self.config.diffusion_model_root
+            sub_dir_name = f"{self.config.shots_num}_{self.config.method}"
+            load_path = os.path.join(load_root, model_type, sub_dir_name, load_name)
 
-    # 扩增数据集
-    augment_data = []
-    augment_attribute = []
-    for attribute in attributes:
-        attribute = attribute.unsqueeze(0)  # [1, 4]
-        # TODO 这里对于每一个属性使用同一个diffusion model进行生成样本，是否有问题？
-        data = diffusion_model.sample_loop(model, augment_size, attribute=attribute)
-        augment_attribute.append(attribute.expand(100, 4).numpy())
-        augment_data.append(data.numpy())
-    augment_data = np.concatenate(augment_data, axis=0)  # [100*class_num, dim]
-    augment_attribute = np.concatenate(augment_attribute, axis=0)  # [100*class_num, 4]
+            # 加载预测模型
+            model = ConcatModel(dim_in=dim_in, dim_condition=dim_condition)
 
-    augment_dict = {
-        "data": augment_data,
-        "attribute": augment_attribute
-    }
+            model_dict = torch.load(load_path)
+            model_state = model_dict['model_state_dict']
+            model.load_state_dict(model_state)
 
-    # 保存生成的数据
-    save_augment_root = config.save_augment_root
-    save_augment_name = f"{model_type}3_standard_augment_num_{config.augment_num}.pkl"
-    save_augment_path = os.path.join(save_augment_root, save_augment_name)
-    with open(save_augment_path, 'wb') as save_file:
-        dill.dump(augment_dict, save_file)
+        elif model_type == 'SelfAttentionModel':
+            load_name = f'epoch{self.epoch}_checkpoint.pkl'
+            load_root = self.config.diffusion_model_root
+            sub_dir_name = f"{self.config.shots_num}_{self.config.method}"
+            load_path = os.path.join(load_root, model_type, sub_dir_name, load_name)
+
+            # 加载预测模型
+            model = AttentionModel(dim_in=dim_in, dim_condition=dim_condition)
+
+            model_dict = torch.load(load_path)
+            model_state = model_dict['model_state_dict']
+            model.load_state_dict(model_state)
+        else:
+            raise ValueError("There is no such model_type, please try another type!")
+
+        # 加载扩散模型
+        diffusion_model = DiffusionModel(self.config)
+
+        # 每一个属性需要扩增的数据的大小
+        if self.config.method == 'Split Standard Dim3 PCA' or self.config.method == 'Split LDA Standard Dim3':
+            augment_size = (self.config.augment_num, 17, dim_in)
+        else:
+            augment_size = (self.config.augment_num, dim_in)
+
+        # # 生成所有类别
+        # attributes = []
+        # cooler, valve, pump, hydraulic = self.config.information
+        # att_iter = product(cooler, valve, pump, hydraulic)
+        # for item in att_iter:
+        #     attributes.append(list(item))
+        # attributes = np.array(attributes)
+        # attributes = attribute_standard(attributes, self.config.information)  # 归一化属性
+        # attributes = torch.FloatTensor(attributes)
+        # # attributes = attributes[:5]  # TODO 只生成前5个类别
+        attributes = torch.FloatTensor(ways)  # 需要生成的类别
+        self.logger.info(f"Generate Attributes: {attributes}")
+
+        # 扩增数据集
+        augment_data = []
+        augment_attribute = []
+        for attribute in attributes:
+            attribute = attribute.unsqueeze(0)  # [1, 4]
+            # TODO 这里对于每一个属性使用同一个diffusion model进行生成样本，是否有问题？
+            data = diffusion_model.sample_loop(model, augment_size, attribute=attribute)
+            augment_attribute.append(attribute.expand(augment_size[0], 4).numpy())
+            augment_data.append(data.cpu().numpy())
+        augment_data = np.concatenate(augment_data, axis=0)  # [100*class_num, dim]
+        augment_attribute = np.concatenate(augment_attribute, axis=0)  # [100*class_num, 4]
+        augment_dict = {
+            "data": augment_data,
+            "attribute": augment_attribute
+        }
+        # print(diffusion_model.sample_list)
+
+        # 保存生成的数据
+        save_augment_root = self.config.augment_data_root
+        save_augment_name = f"{self.config.shots_num}_{self.config.method}_{self.config.augment_num}.pkl"
+        save_augment_path = os.path.join(save_augment_root, save_augment_name)
+        with open(save_augment_path, 'wb') as save_file:
+            dill.dump(augment_dict, save_file)
 
 
 if __name__ == '__main__':
-    data_augment(config, model_type='ConcatLinear')
+    with open("..\\configs\\config_0.yaml") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    config = EasyDict(config)
+
+    path = "..\\processed_data\\test\\augment\\1_Split Standard Dim3 PCA_10.pkl"
+    with open(path, 'rb') as f:
+        data = dill.load(f)
+    print(data["data"].shape)
+    print(data["attribute"].shape)
     # root = config.save_augment_root
     # name = f"augment_num_{config.augment_num}.pkl"
     # path = os.path.join(root, name)

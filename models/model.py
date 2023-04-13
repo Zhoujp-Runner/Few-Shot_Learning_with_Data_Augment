3790904efec9fe63479ca6e4772b5bbf3e5e9bc7
Zhoujp <1228423514@qq.com>
2023-04-13 14:57:42 +0800

    modify the MLPModel, add the AttentionModel and TestModel

--- models/model.py
+++ models/model.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from models.component import ConcatSquashLinear
+from models.component import TransformerLayer
 """
 1. 纯全连接神经网络
 2. 使用ConcatSquashLinear构建的全连接神经网络
@@ -18,11 +19,7 @@ class MLPModel(nn.Module):
         # 网络的线性层
         self.linear_layers = nn.ModuleList(
             [
-                nn.Linear(input_dim, 128),
-                nn.ReLU(),
-                nn.Linear(128, 256),
-                nn.ReLU(),
-                nn.Linear(256, 128),
+                nn.Linear(input_dim + 4, 128),
                 nn.ReLU(),
                 nn.Linear(128, input_dim)
             ]
@@ -30,8 +27,6 @@ class MLPModel(nn.Module):
         # 时间embedding层
         self.time_embeddings = nn.ModuleList(
             [
-                nn.Embedding(num_steps, 128),
-                nn.Embedding(num_steps, 256),
                 nn.Embedding(num_steps, 128)
             ]
         )
@@ -55,35 +50,47 @@ class MLPModel(nn.Module):
         :param attribute: [batch_size, attribute_dim] 注意这里的attribute_dim的维度为4，不考虑第五个维度
         :return: [batch_size, dim]
         """
-        if attribute is None:
-            for idx, embedding in enumerate(self.time_embeddings):
-                t_embedding = embedding(t)  # [batch_size, 1, hidden_dim]
-                t_embedding = t_embedding.squeeze(1)  # [batch_size, hidden_dim]
-                x = self.linear_layers[2 * idx](x)  # [batch_size, hidden_dim]
-                x = x + t_embedding  # [batch_size, hidden_dim]
-                x = self.linear_layers[2 * idx + 1](x)  # [batch_size, hidden_dim]
-        else:
-            for idx, embedding in enumerate(self.time_embeddings):
-                t_embedding = embedding(t)  # [batch_size, 1, hidden_dim]
-                t_embedding = t_embedding.squeeze(1)  # [batch_size, hidden_dim]
-                att_embedding = self.attribute_embedding_linear[idx](attribute)  # [batch_size, hidden_dim]
-                # att_embedding = att_embedding.squeeze(1)  # [batch_size, hidden_dim]  使用的是Linear， 没必要添加这一行
-                x = self.linear_layers[2 * idx](x)  # [batch_size, hidden_dim]
-                x = x + t_embedding + att_embedding  # [batch_size, hidden_dim]
-                x = self.linear_layers[2 * idx + 1](x)  # [batch_size, hidden_dim]
-        x = self.linear_layers[-1](x)  # [batch_size, hidden_dim]
-        return x  # [batch_size, input_dim]
+        # if attribute is None:
+        #     for idx, embedding in enumerate(self.time_embeddings):
+        #         t_embedding = embedding(t)  # [batch_size, 1, hidden_dim]
+        #         t_embedding = t_embedding.squeeze(1)  # [batch_size, hidden_dim]
+        #         x = self.linear_layers[2 * idx](x)  # [batch_size, hidden_dim]
+        #         x = x + t_embedding  # [batch_size, hidden_dim]
+        #         x = self.linear_layers[2 * idx + 1](x)  # [batch_size, hidden_dim]
+        # else:
+        #     for idx, embedding in enumerate(self.time_embeddings):
+        #         t_embedding = embedding(t)  # [batch_size, 1, hidden_dim]
+        #         t_embedding = t_embedding.squeeze(1)  # [batch_size, hidden_dim]
+        #         att_embedding = self.attribute_embedding_linear[idx](attribute)  # [batch_size, hidden_dim]
+        #         # att_embedding = att_embedding.squeeze(1)  # [batch_size, hidden_dim]  使用的是Linear， 没必要添加这一行
+        #         x = self.linear_layers[2 * idx](x)  # [batch_size, hidden_dim]
+        #         x = x + t_embedding + att_embedding  # [batch_size, hidden_dim]
+        #         x = self.linear_layers[2 * idx + 1](x)  # [batch_size, hidden_dim]
+        # x = self.linear_layers[-1](x)  # [batch_size, hidden_dim]
+
+        batch_size = x.shape[0]
+        if attribute.shape[0] == 1:
+            attribute = attribute.expand(batch_size, 4)
+        input = torch.cat([x, attribute], dim=-1)
+        out = self.linear_layers[0](input)
+        t_embedding = self.time_embeddings[0](t)  # [batch_size, 1, hidden_dim]
+        t_embedding = t_embedding.squeeze(1)  # [batch_size, hidden_dim]
+        out = out + t_embedding
+        out = self.linear_layers[1](out)
+        out = self.linear_layers[2](out)
+
+        return out  # [batch_size, input_dim]
 
 
 class ConcatModel(nn.Module):
-    def __init__(self, dim_condition):
+    def __init__(self, dim_in, dim_condition):
         super(ConcatModel, self).__init__()
-        self.concat1 = ConcatSquashLinear(64, 128, dim_condition + 3)
+        self.concat1 = ConcatSquashLinear(dim_in, 128, dim_condition + 3)
         self.concat2 = ConcatSquashLinear(128, 256, dim_condition + 3)
-        self.concat3 = ConcatSquashLinear(256, 512, dim_condition + 3)
-        self.concat4 = ConcatSquashLinear(512, 256, dim_condition + 3)
+        # self.concat3 = ConcatSquashLinear(256, 512, dim_condition + 3)
+        # self.concat4 = ConcatSquashLinear(512, 256, dim_condition + 3)
         self.concat5 = ConcatSquashLinear(256, 128, dim_condition + 3)
-        self.concat6 = ConcatSquashLinear(128, 64, dim_condition + 3)
+        self.concat6 = ConcatSquashLinear(128, dim_in, dim_condition + 3)
         self.type = "ConcatLinear"
 
     def forward(self, x, t, attribute):
@@ -92,16 +99,17 @@ class ConcatModel(nn.Module):
         :param t: 时间t [batch_size, 1]
         :param attribute: 属性 batch_size, attribute_dim]
         """
+        batch_size = x.shape[0]
         if attribute.shape[0] == 1:
-            attribute = attribute.expand(100, 4)
+            attribute = attribute.expand(batch_size, 4)
 
         time_emb = torch.cat([t, torch.sin(t), torch.cos(t)], dim=-1)  # [batch_size, 3]
         condition_emb = torch.cat([time_emb, attribute], dim=-1)  # [batch_size, attribute_dim + 3]
 
         out = self.concat1(x, condition_emb)
         out = self.concat2(out, condition_emb)
-        out = self.concat3(out, condition_emb)
-        out = self.concat4(out, condition_emb)
+        # out = self.concat3(out, condition_emb)
+        # out = self.concat4(out, condition_emb)
         out = self.concat5(out, condition_emb)
         out = self.concat6(out, condition_emb)
 
@@ -109,7 +117,92 @@ class ConcatModel(nn.Module):
         return out
 
 
+class AttentionModel(nn.Module):
+    def __init__(self, dim_in, dim_condition):
+        super(AttentionModel, self).__init__()
+        self.concat = ConcatSquashLinear(dim_in, dim_in*2, dim_condition+3)
+        self.transformer_encoder = TransformerLayer(dim_in*2, dim_in*4, dim_in*4)
+        self.linear = ConcatSquashLinear(dim_in*2, dim_in, dim_condition+3)
+
+        self.type = 'SelfAttentionModel'
+
+    def forward(self, x, t, attribute):
+
+        batch_size = x.shape[0]
+        if attribute.shape[0] == 1:
+            attribute = attribute.expand(batch_size, 1, 4)
+        if len(attribute.shape) == 2:
+            attribute = attribute.unsqueeze(1)
+
+        if len(t.shape) == 2:
+            t = t.unsqueeze(1)
+
+        time_emb = torch.cat([t, torch.sin(t), torch.cos(t)], dim=-1)  # [batch_size, 1, 3]
+        condition_emb = torch.cat([time_emb, attribute], dim=-1)  # [batch_size, 1, attribute_dim + 3]
+
+        concat = self.concat(x, condition_emb)
+        context = self.transformer_encoder(concat)
+        result = self.linear(context, condition_emb)
+
+        return result
+
+
+class TestModel(nn.Module):
+
+    def __init__(self, n_steps, num_unit=128):
+        """
+        初始化类
+        :param num_unit: 隐藏层神经元的数量
+        """
+        super(TestModel, self).__init__()
+        # 线性层组合
+        self.linears = nn.ModuleList(
+            [
+                nn.Linear(2, num_unit),
+                nn.ReLU(),
+                nn.Linear(num_unit, num_unit),
+                nn.ReLU(),
+                nn.Linear(num_unit, num_unit),
+                nn.ReLU(),
+                nn.Linear(num_unit, 2)
+            ]
+        )
+        # embedding层组合，不同深度上权重不一样，增加参数但不增加计算量
+        self.step_embeddings = nn.ModuleList(
+            [
+                nn.Embedding(n_steps, num_unit),
+                nn.Embedding(n_steps, num_unit),
+                nn.Embedding(n_steps, num_unit)
+            ]
+        )
+
+    def forward(self, x, t):
+        """
+        :param x: [batch_size, 2]
+        :param t: [batch_size, 1]
+        :return: [batch_size, 2]
+        """
+        for idx, embedding in enumerate(self.step_embeddings):
+            t_embedding = embedding(t)  # [batch_size, 1, num_unit]
+            t_embedding = t_embedding.squeeze(1)  # [batch_size, num_unit]
+            x = self.linears[2 * idx](x)  # [batch_size, num_unit]
+            x += t_embedding
+            x = self.linears[2 * idx + 1](x)
+        x = self.linears[-1](x)
+        return x
+
+
+class TorchAttentionModel(nn.Module):
+    pass
+
+
 if __name__ == '__main__':
-    m = ConcatModel(4)
-    print(m)
+    model = AttentionModel(64, 4)
+    input = torch.randn(32, 17, 64)
+    attribute = torch.randn(32, 4)
+    if len(attribute.shape) == 2:
+        attribute = attribute.unsqueeze(1)
+    t = torch.randn(32, 1, 1)
+    out = model(input, t, attribute)
+    print(out.shape)
 

ad30568b45efbe35e330240f7693636ba2222369
Zhoujp <1228423514@qq.com>
2023-05-08 21:59:26 +0800

    modify the log and recurrently running the code under the same condition

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ import logging
 
 from process_data.dataset import FaultDataset
 from models.diffusion import DiffusionModel
-from models.model import MLPModel, ConcatModel, AttentionModel
+from models.model import MLPModel, ConcatModel, AttentionModel, AdaModel
 from process_data.augment import DataAugment
 from models.classification import TrainClassification
 
@@ -76,7 +76,7 @@ def get_dataset(config, mode='train', method='LDA', augment=False):
 
 def set_log():
     """设置log文件"""
-    logger = logging.getLogger("AugmentLog")
+    logger = logging.getLogger("TestLog")
     logger.setLevel(logging.DEBUG)
 
     # 清空该log的句柄
@@ -101,53 +101,116 @@ if __name__ == '__main__':
     test_config_path = "configs\\test_config.yaml"
     all_config = get_all_config(test_config_path)
     configs = get_config_list(all_config)
+    test_times = 10
     epochs = np.array(range(10 - 1, 2000, 10))
     index = np.argmin(np.abs(epochs - 1534))
-    # test_logger = set_log()
-    # min_epochs = [1725, 1638, 1649, 1983, 1919, 1934, 1794, 1863, 1999, 1981, 1969, 1914, 1895, 1907, 1741, 1981, 1988, 1996, 1986, 1989]
+    test_logger = set_log()
+    test_logger.info(f"total_time: {test_times}")
+
     for idx, config in enumerate(configs):
-        print("=========================Diffusion Training=========================")
-        config = EasyDict(config)
-        diffusion_model = DiffusionModel(config)
 
-        diffusion_dataset = FaultDataset(config, method=config.method)
-        dim_in = diffusion_dataset.train_data.shape[-1]
-        dim_condition = diffusion_dataset.train_attribute.shape[-1]
-        linear_model = MLPModel(input_dim=dim_in, num_steps=diffusion_model.num_diffusion_steps)
-        concat_model = ConcatModel(dim_in=dim_in, dim_condition=dim_condition)
-        attention_model = AttentionModel(dim_in=dim_in, dim_condition=dim_condition)
+        # 准确率之和
+        total_augment_accuracy = 0
+        total_source_accuracy = 0
+        # 最小的准确率
+        min_augment_accuracy = 2
+        min_source_accuracy = 2
+        # 对应最小准确率的ways
+        min_augment_ways = None
+        min_source_ways = None
+        # 对应最小准确率的次数
+        min_source_time = 0
+        min_augment_time = 0
 
-        min_loss, min_epoch = diffusion_model.train(diffusion_dataset, linear_model)
-        print("=========================Training done==========================")
-        # diffusion_message = f"Diffusion Training : min_epoch = {min_epoch} : min_loss = {min_loss}"
-        # test_logger.info(diffusion_message)
-        # print(diffusion_message)
+        for time in range(test_times):
+            print("=========================Diffusion Training=========================")
+            config = EasyDict(config)
+            diffusion_model = DiffusionModel(config)
 
-        # epochs = np.array(range(config.checkpoint_interval-1, config.epochs, config.checkpoint_interval))
-        # index = np.argmin(np.abs(epochs - min_epoch))
-        # epoch = epochs[index]
+            diffusion_dataset = FaultDataset(config,
+                                             method=config.method,
+                                             use_random_combination=True)
+            dim_in = diffusion_dataset.train_data.shape[-1]
+            dim_condition = diffusion_dataset.train_attribute.shape[-1]
+            linear_model = MLPModel(input_dim=dim_in,
+                                    num_steps=diffusion_model.num_diffusion_steps)
+            concat_model = ConcatModel(dim_in=dim_in,
+                                       dim_condition=dim_condition)
+            attention_model = AttentionModel(dim_in=dim_in,
+                                             dim_condition=dim_condition)
+            ada_model = AdaModel(dim_in=dim_in,
+                                 dim_hidden=128,
+                                 attribute_dim=dim_condition,
+                                 num_steps=diffusion_model.num_diffusion_steps)
 
-        # # min_epoch = min_epochs[idx]
-        print("=========================Augment===========================")
-        augment = DataAugment(config, min_epoch)
-        augment.data_augment(dim_in=dim_in,
-                             dim_condition=dim_condition,
-                             model_type=linear_model.type,
-                             ways=diffusion_dataset.ways)
-        print("=========================Augment done===========================")
+            min_loss, min_epoch = diffusion_model.train(diffusion_dataset, ada_model, time)
+            print("=========================Training done==========================")
+            # diffusion_message = f"Diffusion Training : min_epoch = {min_epoch} : min_loss = {min_loss}"
+            # test_logger.info(diffusion_message)
+            # print(diffusion_message)
 
-        print("=========================Classification===========================")
-        classification = TrainClassification(config)
-        result = classification.train_loop()
+            # epochs = np.array(range(config.checkpoint_interval-1, config.epochs, config.checkpoint_interval))
+            # index = np.argmin(np.abs(epochs - min_epoch))
+            # epoch = epochs[index]
 
-        # # 记录结果
-        # message = f"{config.shots_num}_{config.method} classification result:"
-        # message_no_augment = f"Without Augment: epoch{result[0][0]}---accuracy={result[0][1]}"
-        # message_augment = f"Without Augment: epoch{result[1][0]}---accuracy={result[1][1]}"
-        # test_logger.info(message)
-        # test_logger.info(message_no_augment)
-        # test_logger.info(message_augment)
-        print("=========================Classification done===========================")
+            # # min_epoch = min_epochs[idx]
+            print("=========================Augment===========================")
+            augment = DataAugment(config, min_epoch)
+            augment.data_augment(dim_in=dim_in,
+                                 dim_condition=dim_condition,
+                                 model_type=ada_model.type,
+                                 ways=diffusion_dataset.ways,
+                                 time=time)
+            print("=========================Augment done===========================")
+
+            print("=========================Classification===========================")
+            classification = TrainClassification(config,
+                                                 ways=diffusion_dataset.ways)
+            result = classification.train_loop(time)
+            if min_augment_accuracy > result[1][1]:
+                min_augment_accuracy = result[1][1]
+                min_augment_ways = diffusion_dataset.ways
+                min_augment_time = time
+            if min_source_accuracy > result[0][1]:
+                min_source_accuracy = result[0][1]
+                min_source_ways = diffusion_dataset.ways
+                min_source_time = time
+
+            total_augment_accuracy += result[1][1]
+            total_source_accuracy += result[0][1]
+
+            # # 记录结果
+            # message = f"{config.shots_num}_{config.method} classification result:"
+            # message_no_augment = f"Without Augment: epoch{result[0][0]}---accuracy={result[0][1]}"
+            # message_augment = f"Without Augment: epoch{result[1][0]}---accuracy={result[1][1]}"
+            # test_logger.info(message)
+            # test_logger.info(message_no_augment)
+            # test_logger.info(message_augment)
+            print("=========================Classification done===========================")
+        average_augment_accuracy = total_augment_accuracy / test_times
+        average_source_accuracy = total_source_accuracy / test_times
+        print(f"augment_num: {config.augment_num}")
+        print(f"test_times: {test_times}")
+        print(f"average_source_accuracy: {average_source_accuracy}")
+        print(f"min_source_accuracy: {min_source_accuracy}")
+        print(f"min_source_ways: {min_source_ways}")
+        print(f"average_augment_accuracy: {average_augment_accuracy}")
+        print(f"min_augment_accuracy: {min_augment_accuracy}")
+        print(f"min_augment_ways: {min_augment_ways}")
+
+        test_logger.info(f"------------------Average Result------------------")
+        test_logger.info(f"shots_num: {config.shots_num}")
+        test_logger.info(f"ways_num: {config.ways_num}")
+        test_logger.info(f"method: {config.method}")
+        test_logger.info(f"augment_num: {config.augment_num}")
+        test_logger.info(f"test_times: {test_times}")
+        test_logger.info(f"average_source_accuracy: {average_source_accuracy}")
+        test_logger.info(f"min_source_accuracy: {min_source_accuracy}")
+        test_logger.info(f"min_source_ways: {min_source_ways}")
+        test_logger.info(f"average_augment_accuracy: {average_augment_accuracy}")
+        test_logger.info(f"min_augment_accuracy: {min_augment_accuracy}")
+        test_logger.info(f"min_augment_ways: {min_augment_ways}")
+        test_logger.info(f"------------------Average Result------------------")
 
 
 
